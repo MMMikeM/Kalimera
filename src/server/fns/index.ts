3c97a91ad4d59921a7f259dee721d112fd1e8aa5
Mike Murray <mikem.tba@gmail.com>
2026-05-20 08:25:38 +0300

    fix(build): exclude getVerbConjugationQuestions from client barrel

--- src/server/fns/index.ts
+++ src/server/fns/index.ts
@@ -5,4 +5,10 @@ export * from "./noun";
 export * from "./progress";
 export * from "./search";
 export * from "./srs";
-export * from "./verbs";
+export {
+	getVerbDrillQuestionsFn,
+	getPresentSg1QuestionsFn,
+	getAoristDrillQuestionsFn,
+	getAoristSg1QuestionsFn,
+	getFutureDrillQuestionsFn,
+} from "./verbs";
